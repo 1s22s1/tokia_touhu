f806cf762eaa0832f44b17c80a911ed3479c0fed
1s22s1 <1s22s1@users.noreply.github.com>
2025-06-29 13:54:46 +0900

    :up:

---
+++ 20250629/359.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n'
+#define ll long long
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+
+int init()
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    return 0;
+}
+
+int main()
+{
+    init();
+
+    ll N;
+    cin >> N;
+
+    cout << N / 5 + N % 5 << endl;
+
+    return 0;
+}
